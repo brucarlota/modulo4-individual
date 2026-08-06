52ee349e2db060fd6f8d97a69b7affe9b3d0ac65
Bruna Carlota <bru.carlota@yahoo.com.br>
2026-08-06 08:42:42 -0300

    add exercicio 02

---
+++ Exercicio02/Program.cs
@@ -0,0 +1,9 @@
+﻿// Exercício 02 - Antecessor e sucessor
+
+Console.WriteLine("Escreva um número inteiro");
+
+int.TryParse(Console.ReadLine(), out int n);
+
+Console.WriteLine($"Antecessor {n-1}");
+Console.WriteLine($"Número informado {n}");
+Console.WriteLine($"Sucessor {n + 1}");
