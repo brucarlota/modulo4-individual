eeca9f01e3b809317189648a81e929c392bb11ca
Bruna Carlota <bru.carlota@yahoo.com.br>
2026-08-08 13:13:12 -0300

    add exercicio 05

---
+++ Exercicio05/Program.cs
@@ -0,0 +1,18 @@
+﻿// Calculadora simples
+
+decimal a;
+decimal b;
+Console.WriteLine("Digite dois valores");
+
+Console.Write("Valor A: ");
+decimal.TryParse(Console.ReadLine(), out a);
+
+Console.Write("Valor B: ");
+decimal.TryParse(Console.ReadLine(), out b);
+
+Console.WriteLine();
+Console.WriteLine("---- Calculadora Simples ----");
+Console.WriteLine($"Soma: {a+b:F2}");
+Console.WriteLine($"Subtração: {a-b:F2}");
+Console.WriteLine($"Multiplicação: {a*b:F2}");
+Console.WriteLine($"Divisão: {a/b:F2}");
