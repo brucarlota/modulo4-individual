b953d1c0928fcf63df80b9e8206fa2e271c60b21
Bruna Carlota <bru.carlota@yahoo.com.br>
2026-08-14 19:49:30 -0300

    add exercicio 08

---
+++ Exercicio08/Program.cs
@@ -0,0 +1,8 @@
+﻿// Tabuada
+
+Console.WriteLine("Escreva um número inteiro");
+int.TryParse(Console.ReadLine(), out int n);
+
+for (int i = 0; i <= 10; i++) {
+	Console.WriteLine($"{n} x {i} = {n*i}");
+}
