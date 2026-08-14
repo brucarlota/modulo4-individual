2513ca0b84468b4a755efe4fc19badefce5e58c8
Bruna Carlota <bru.carlota@yahoo.com.br>
2026-08-14 20:14:54 -0300

    add exercicio 10

---
+++ Exercicio10/Program.cs
@@ -0,0 +1,28 @@
+﻿// Login simples
+
+Dictionary<string, string> usuario = new()
+{
+    { "brunacruz", "123456senha" },
+		{ "mariajose", "09876543haha" }
+};
+
+Console.Write("Digite usuário: ");
+string userInput = Console.ReadLine()!;
+
+if (!usuario.ContainsKey(userInput))
+{
+    Console.WriteLine("Usuário não encontrado.");
+    return;
+}
+
+Console.Write("Digite senha: ");
+string passwordInput = Console.ReadLine()!;
+
+while (passwordInput != usuario[userInput]) {
+	Console.Write("Senha errada! Tente novamente: ");
+	passwordInput = Console.ReadLine()!;
+}
+
+if (passwordInput == usuario[userInput]) {
+	Console.Write("Login Sucesso!!");
+}
