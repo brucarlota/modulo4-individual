20ae0155ea14de9895bcb22c97824f9b19175548
Bruna Carlota <bru.carlota@yahoo.com.br>
2026-08-15 10:10:32 -0300

    add exercicios 11 e 12

---
+++ Exercicio11/Program.cs
@@ -0,0 +1,13 @@
+﻿// Soma até digitar zero
+
+int soma = 0;
+int numero;
+
+Console.WriteLine("Digite números para somar. Digite 0 para obter o resultado:");
+do {
+	numero = int.Parse(Console.ReadLine());
+	soma += numero;
+
+} while (numero != 0);
+
+Console.WriteLine($"Soma = {soma}");
---
+++ Exercicio12/Program.cs
@@ -0,0 +1,11 @@
+﻿// Contador de caracteres
+
+Console.Write("Digite um nome: ");
+string nome = Console.ReadLine()!;
+
+while (nome.Length == 0) {
+	Console.Write("Digite um nome válido: ");
+	nome = Console.ReadLine()!;
+}
+
+Console.WriteLine($"O nome digitado possui {nome.Length} caracteres");
